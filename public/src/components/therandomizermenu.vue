a3a5cf3313fb3d9f774efb6c73ba6c8dffb9b381
TopRamen789 <TopRamen789@gmail.com>
2020-06-26 01:12:22 -0600

    Rename and move stuff, stub the randomizer menu.

---
+++ public/src/components/therandomizermenu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button id="randomizeButton" v-onClick="randomize"></button>
+        <button id=""></button>
+        <button id=""></button>
+        <button id=""></button>
+    </div>
+</template>
+
+<script>
+import randomizer from '../randomizer';
+
+export default {
+    methods: {
+        randomize: function() {
+            randomizer.randomize();
+        }
+    }
+}
+
+</script>
