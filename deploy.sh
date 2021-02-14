24966ca4e0f42a943aee4f104a9c5391ff72b7cb
TopRamen789 <TopRamen789@gmail.com>
2021-02-13 17:05:13 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:topramen789/DominionRandomizer.git master:gh-pages
+git push -f topramen789@github.com:topramen789/DominionRandomizer.git master:gh-pages
 cd -
